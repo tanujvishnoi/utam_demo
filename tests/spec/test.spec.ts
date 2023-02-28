09123473bb1284c7ada66598bb584df70c3c918f
Tanuj Vishnoi <tanuj.vishnoi@bullhorn.com>
2023-02-28 11:56:20 +0100

    modified test to get specific error

--- tests/spec/test.spec.ts
+++ tests/spec/test.spec.ts
@@ -1,10 +1,68 @@
 import demo from "@utam-pageObjects/demo"
 
-describe("reproduce truncate error stack", ()=>{
+describe("reproduce truncate error stack", () => {
 
-    it("try to perform task on invalid elements", async ()=>{
+
+    /**
+     * Getting non-existing element directly 
+     */
+    it("Case 1: Getting no existing element directly using getter ", async () => {
+        await browser.url("https://www.shopsite.com/demo.html")
+        let demoPage = await utam.load(demo)
+        let input = await demoPage.getUserNameInput()
+    })
+
+
+
+    /**
+    * Getting non-existing element using utam method 
+    */
+    it("Case 2: Getting no existing element inside a method", async () => {
+        await browser.url("https://www.shopsite.com/demo.html")
+        let demoPage = await utam.load(demo)
+        await demoPage.clickUserName()
+    })
+
+    /**
+     * 
+     * Getting non-existing element inside a predicate
+     */
+
+    it("Case 3: Getting no existing inside a predicate", async () => {
         await browser.url("https://www.shopsite.com/demo.html")
         let demoPage = await utam.load(demo)
         await demoPage.login()
     })
+
+    /**
+     * 
+     * wait for hidden element visibility 
+     */
+
+    it("Case 4: Wait for visibility of hidden search input", async () => {
+        await browser.url("https://www.shopsite.com/demo.html")
+        let demoPage = await utam.load(demo)
+        await demoPage.waitForSearchInputToVisible()
+    })
+
+    
+    /**
+     * 
+     * get hidden element then wait for visibility 
+     */
+
+    it("Case 5: Get hidden element ,then wait for visibility", async () => {
+        await browser.url("https://www.shopsite.com/demo.html")
+        let demoPage = await utam.load(demo)
+        let searchInput = await demoPage.getSearchInput()
+        await searchInput.waitForVisible();
+        await searchInput.waitFor(()=>{
+            searchInput.waitForVisible()
+        })
+       
+    })
+
+
+
+
 })
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -162,7 +162,7 @@ export const config: Options.Testrunner = {
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 45_000,
+    waitforTimeout: 10_000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
